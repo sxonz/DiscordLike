00c95754b604d8dcd675db66f67643a35728ab94
Kuh-122 <Kimeh20091021@gmail.com>
2025-12-23 20:04:32 +0900

    내가 흑마술사를 처단했다 으하하

--- Assets/Script/Player/PlayerAim.cs
+++ Assets/Script/Player/PlayerAim.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 public class PlayerAim : MonoBehaviourPun
 {
     Camera cam;
+    public float angleOffset = -90f;
 
     void Start()
     {
         cam = Camera.main;
     }
-
     void Update()
     {
         if (!photonView.IsMine) return;
+
         Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = mousePos - (Vector2)transform.position;
 
-        transform.up = dir;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        Debug.Log(angle + angleOffset);
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + angleOffset);
     }
 }
--- Assets/Script/Player/Rotate.cs
+++ Assets/Script/Player/Rotate.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using Photon.Pun;
-public class Rotate : MonoBehaviourPun { 
-    private Camera camera; 
-    void Start() { 
-        camera = Camera.main; 
+
+public class Rotate : MonoBehaviourPun
+{
+    Camera cam;
+    public float angleOffset = -90f; // 90도 틀어지면 -90, 반대면 +90. 45면 -45 등으로 조절
+
+    void Start()
+    {
+        cam = Camera.main;
     }
-        void Update() {
+
+    void Update()
+    {
         if (!photonView.IsMine) return;
-        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 dirVec = mousePos - (Vector2)transform.position; transform.up = dirVec.normalized; 
-    } 
+
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = mousePos - (Vector2)transform.position;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + angleOffset);
+    }
 }
