f6c28c4bdc86c020b4c7fdd3c0753d2a5070e3d5
Kuh-122 <Kimeh20091021@gmail.com>
2025-12-23 16:50:19 +0900

    tlqkf 인스턴스 삭제

--- Assets/Script/Weapon/Bow.cs
+++ Assets/Script/Weapon/Bow.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class Bow : Weapon
 {
-    public GameObject arrowPrefab;
+    public string arrowPrefabName = "Arrow"; 
     public Transform firePoint;
     public float arrowSpeed = 12f;
 
     public override void SpecialAttack()
     {
-        GameObject arrow = Instantiate(
-            arrowPrefab,
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        GameObject arrow = PhotonNetwork.Instantiate(
+            arrowPrefabName,
             firePoint.position,
             firePoint.rotation
         );
 
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
         if (rb != null)
+        {
             rb.linearVelocity = firePoint.up * arrowSpeed;
+        }
     }
-
 }
